8d66bcc470fe1f484f1fc5a6ad527168b230a93a
CyrusN <CyrusN@CyrusN.local>
2017-08-14 14:29:44 +0800

    update voice.js

--- server/handler/voice.js
+++ server/handler/voice.js
@@ -1,12 +1,23 @@
 const getVocabs = require('../helper.js').getVocabs;
+const googleTTS = require('google-tts-api');
+const request = require('request')
 
 function voiceHandler(req, res) {
+  // will pronouce the title of the vocab by given vocab id
+  // use "google-tts-api"
   const assessmentName = req.params.name
   const index = parseInt(req.params.index)
+
   const vocabs = getVocabs(assessmentName)
 
-  const vocab = Object.assign({}, vocabs[index])
-  res.json(vocab[index].title)
-}
+  const vocab = vocabs[index]
 
+  googleTTS(vocab.title, 'en', 0.24)
+    .then(function(url) {
+  		request(url).pipe(res)
+    })
+    .catch(function(err) {
+      res.send(err)
+    })
+}
 module.exports = voiceHandler;
