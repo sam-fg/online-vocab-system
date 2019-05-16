aeed2f2beb540bc66f1fe742187af9cc4e1d9260
cyrusn <cyrusncy@gmail.com>
2019-05-16 11:57:08 +0800

    prevent copy and paste

--- html/src/pages/test.vue
+++ html/src/pages/test.vue
@@ -12,7 +12,7 @@
 
       <div class="card-body text-info">  <!-- 3 -->
         <div class="card-block">
-            <h2>
+            <h2 onmousedown='return false;' onselectstart='return false;'>
               Q{{index+1}}/{{$root.vocabids.length}}: {{msg}}
             </h2>
             <hr>
@@ -27,6 +27,7 @@
               class="form-control"
               v-model="engAns"
               placeholder=''
+              onpaste="return false"
               @keypress.enter.prevent="checkAns"
               />
             </div>
@@ -56,37 +57,38 @@
 </template>
 
 <script>
-import axios from 'axios'
-import _ from 'lodash'
+import axios from "axios"
+import _ from "lodash"
 
 export default {
-  data () {
+  data() {
     return {
-      msg: '_______', // @Test
+      msg: "_______", // @Test
       mistakes: 0,
       index: 0, // @Test
       isCorrect: false, // @Test
-      engAns: '', // @Test
-      voiceUrl: '' // @Test
+      engAns: "", // @Test
+      voiceUrl: "" // @Test
       // vocabids: [], //@Test
     }
   },
   computed: {
-    mode () {
+    mode() {
       switch (this.$root.selectedMode) {
-        case '1':
-          return 'Normal'
-        case '2':
-          return 'At least 50% correct'
-        case '3':
-          return 'At least 75% correct'
+        case "1":
+          return "Normal"
+        case "2":
+          return "At least 50% correct"
+        case "3":
+          return "At least 75% correct"
       }
     }
   },
-  mounted () {
+  mounted() {
     const vm = this
-    document.getElementById('ans').focus()
-    axios.get('./api/assessment/' + vm.$root.assessmentName + '/size')
+    document.getElementById("ans").focus()
+    axios
+      .get("./api/assessment/" + vm.$root.assessmentName + "/size")
       .then(response => {
         vm.$root.vocabids = _.shuffle(_.range(response.data))
         // ref. https://lodash.com/docs/4.17.4#range
@@ -97,18 +99,26 @@ export default {
       })
   },
   methods: {
-    onStart () {
+    onStart() {
       const vm = this
       vm.index = -1
       vm.$root.wrongVocabs = []
       vm.getNextVocab()
     },
-    checkAns () {
+    checkAns() {
       const vm = this
       const id = vm.$root.vocabids[vm.index]
       const ans = vm.engAns.trim()
-      axios.get('./api/check/assessment/' + vm.$root.assessmentName + '/index/' + id + '?ans=' + ans)
-        .then(function (response) {
+      axios
+        .get(
+          "./api/check/assessment/" +
+            vm.$root.assessmentName +
+            "/index/" +
+            id +
+            "?ans=" +
+            ans
+        )
+        .then(function(response) {
           vm.isCorrect = response.data
 
           if (!vm.isCorrect) {
@@ -122,20 +132,26 @@ export default {
           vm.getNextVocab()
         }) // function(response) end
     }, // checkAns() end
-    getVoiceSrc () {
+    getVoiceSrc() {
       const vm = this
-      const url = './api/voice/assessment/' + vm.$root.assessmentName + '/index/' + vm.$root.vocabids[vm.index] + '?speed=0.24'
+      const url =
+        "./api/voice/assessment/" +
+        vm.$root.assessmentName +
+        "/index/" +
+        vm.$root.vocabids[vm.index] +
+        "?speed=0.24"
       vm.voiceUrl = url
       vm.$refs.player.load()
     },
-    voiceOut () {
+    voiceOut() {
       const vm = this
       // const url = 'https://careers.liping.edu.hk/edict/api/voice/vocab/id/' + vm.$root.vocabids[vm.index]
       vm.$refs.player.play()
     },
-    getNextVocab () {
+    getNextVocab() {
       const vm = this
-      const wrongPercent = vm.$root.wrongVocabs.length / vm.$root.vocabids.length
+      const wrongPercent =
+        vm.$root.wrongVocabs.length / vm.$root.vocabids.length
 
       var needRestart = false
       switch (parseInt(vm.$root.selectedMode)) {
@@ -150,7 +166,7 @@ export default {
       if (needRestart) {
         // restart test
         /* global alert */
-        alert('Restart Test')
+        alert("Restart Test")
         vm.onStart()
         return
       }
@@ -158,21 +174,27 @@ export default {
       vm.index++
       if (vm.index < vm.$root.vocabids.length) {
         vm.isCorrect = false
-        vm.engAns = ''
+        vm.engAns = ""
         const index = vm.index
 
-        axios.get('./api/assessment/' + vm.$root.assessmentName + '/index/' + vm.$root.vocabids[index])
-          .then(function (response) {
-            const {definition, partOfSpeech} = response.data
+        axios
+          .get(
+            "./api/assessment/" +
+              vm.$root.assessmentName +
+              "/index/" +
+              vm.$root.vocabids[index]
+          )
+          .then(function(response) {
+            const { definition, partOfSpeech } = response.data
             vm.msg = `${definition} (${partOfSpeech})`
 
             vm.getVoiceSrc()
           })
-          .catch(function (error) {
+          .catch(function(error) {
             console.log(error)
           })
       } else {
-        vm.$root.currentRoute = '/report'
+        vm.$root.currentRoute = "/report"
       }
     }
   }
